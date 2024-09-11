fe4b1d5ff400a1dd5f896a9799d5c5afeb90c511
Nicole <nickii.cn@hotmail.com>
2024-09-11 11:56:54 -0300

    funciona registro

--- Backend/controllers/UserController.js
+++ Backend/controllers/UserController.js
@@ -12,33 +12,14 @@ export const registerUser = async (req, res) => {
     }
 
     try {
-        // Verificar si el usuario ya existe
-        const existingUser = await User.findOne({ email });
-        if (existingUser) {
-            return res.status(400).json({ error: 'El usuario ya existe' });
-        }
-
-        // Encriptar la contraseña
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const newUser = new User({
-            name,
-            email,
-            password: hashedPassword,
-            role: role || 'user',
-        });
-
-        // Guardar el nuevo usuario en la base de datos
-        const savedUser = await newUser.save();
-
-        // Generar un token JWT para el nuevo usuario
+        const existingUser = await UserManager.registerUser({ name, email, password, role });
         const token = generateToken({
-            _id: savedUser._id,
-            email: savedUser.email,
-            role: savedUser.role,
+            _id: existingUser._id,
+            email: existingUser.email,
+            role: existingUser.role,
         });
 
-        // Responder con el token y los datos del usuario
-        res.status(201).json({ token, user: savedUser });
+        res.status(201).json({ token, user: existingUser });
     } catch (error) {
         console.error('Error al registrar usuario:', error);
         res.status(500).json({ error: 'Error interno en el servidor', details: error.message });
@@ -49,32 +30,30 @@ export const registerUser = async (req, res) => {
 export const loginUser = async (req, res) => {
     const { email, password } = req.body;
 
-    // Validación de campos requeridos
     if (!email || !password) {
         return res.status(400).json({ message: 'Email y contraseña son obligatorios' });
     }
 
     try {
-        // Buscar al usuario por su email
         const user = await User.findOne({ email });
         if (!user) {
             return res.status(401).json({ message: 'Email o contraseña inválidos' });
         }
 
-        // Verificar la contraseña
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) {
             return res.status(401).json({ message: 'Email o contraseña inválidos' });
         }
 
-        // Generar un token JWT
+        user.last_login = Date.now();
+        await user.save();
+
         const token = generateToken({
             _id: user._id,
             email: user.email,
             role: user.role,
         });
 
-        // Responder con el token y los datos del usuario
         res.status(200).json({ user, token });
     } catch (error) {
         console.error('Error durante el inicio de sesión:', error);
